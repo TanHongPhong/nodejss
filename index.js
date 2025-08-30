b2a8026275348380b7b1f0f14140a1b231e3e1ea
Tan Hong Phong <phongtan.31241026726@st.ueh.edu.vn>
2025-08-30 21:19:07 +0700

    morgan

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
